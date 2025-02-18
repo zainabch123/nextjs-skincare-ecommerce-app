828e45f203da5e7d77dc8ff10f9d76e96c593347
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-02-18 18:22:22 +0000

    basic layout to dashboard including header, nav-bar, and heroImg

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,37 +5,31 @@ import FavouritesIcon from "../../public/icons/favouritesIcon";
 import BasketIcon from "../../public/icons/basketIcon";
 
 export default function Header() {
-    return (
-      <header>
-        <div className="language-options-menu">
-          <a href="#" style={{ color: "black" }}>
-            United Kingdom | English
-          </a>
-        </div>
-        <div className="logo">
-          <GloLogo />
-        </div>
-        <ul className="user-panel">
-          <li
-            className="profile-icon"
-            style={{ color: "black" }}
-          >
-            <ProfileIcon />
-          </li>
-          <li className="search-icon" style={{ color: "black" }}>
-            <SearchIcon />
-          </li>
-          <li className="favourites-icon" style={{ color: "black" }}>
-            <FavouritesIcon />
-          </li>
-          <li
-            className="basket-icon"
-            style={{ color: "black" }}
-          >
-            <BasketIcon />
-          </li>
-          <div className="basket-quantitiy-icon">0</div>
-        </ul>
-      </header>
-    );
+  return (
+    <header>
+      <div className="language-options-menu">
+        <a href="#" style={{ color: "black" }}>
+          United Kingdom | English
+        </a>
+      </div>
+      <div className="logo">
+        <GloLogo />
+      </div>
+      <ul className="user-panel">
+        <li className="profile-icon" style={{ color: "black" }}>
+          <ProfileIcon />
+        </li>
+        <li className="search-icon" style={{ color: "black" }}>
+          <SearchIcon />
+        </li>
+        <li className="favourites-icon" style={{ color: "black" }}>
+          <FavouritesIcon />
+        </li>
+        <li className="basket-icon" style={{ color: "black" }}>
+          <BasketIcon />
+        </li>
+        <div className="basket-quantitiy-icon">0</div>
+      </ul>
+    </header>
+  );
 }
