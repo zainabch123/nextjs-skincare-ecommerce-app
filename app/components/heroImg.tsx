e70e0bd0bc58be02a54b0a855e5974fad348d172
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-02-24 17:10:06 +0000

    updated heroImg sizing

--- app/components/heroImg.tsx
+++ app/components/heroImg.tsx
@@ -3,8 +3,14 @@ import heroImg from "../../public/heroImages/heroImg9.jpg";
 
 export default function HeroImg() {
   return (
-    <section className="hero-image-carousel">
-      <Image className="hero-image" src={heroImg} alt="Skincare Hero Image" />
+    <section className="w-full h-[75vh]">
+      <Image
+        className="w-full h-full object-cover object-left-top"
+        src={heroImg}
+        width={5998}
+        height={2608}
+        alt="Skincare Hero Image"
+      />
     </section>
   );
 }
