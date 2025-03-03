255035932bffdb5c6a23f7e671e696c42bc11c88
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-03-03 14:04:33 +0000

    debugging error

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import productData from "../lib/data";
 
 export default function Carousel() {
-  const carouselRef = useRef<HTMLDivElement>(null);
+  const carouselRef = useRef(null);
 
   const scrollLeft = () => {
     if (carouselRef.current) {
